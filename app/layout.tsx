a6808f3c184fe04332d232870997c36125b61df8
zunzun670 <kazho4zl@gmail.com>
2026-04-30 10:30:34 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,17 @@
+import type { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Emotion Check',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
+}
